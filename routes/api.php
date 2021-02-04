21d54070022f7fca293c7e37387e9aa9cc7e89e4
Waqas Hafeez <waqas.hafeez@virtualforce.io>
2021-02-05 00:17:45 +0500

    adding routes

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('purchaseSubscription','SubscriptionController@create');
+Route::get('subscriptionworker','WorkerController@index');
